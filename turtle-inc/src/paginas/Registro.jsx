eae379a2fa70e5d40b315957451b5332584f32bb
ITSISACO <isaacibarra010saacibarra0102@gmail.com>
2026-06-10 16:09:27 -0500

    secciones

---
+++ turtle-inc/src/paginas/Registro.jsx
@@ -0,0 +1,13 @@
+import Menu from "../componentes/Menu"
+
+function Registro() {
+    return (
+        <div>
+            <Menu/>
+            <h1>Bienvenido a la pagina de Registro</h1>
+        </div>
+    )
+}
+
+export default Registro 
+
